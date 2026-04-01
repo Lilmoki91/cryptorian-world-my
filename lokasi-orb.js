837f8982becdabaac4e6cc1d66bb0a1bd504815a
Johannes Meshchain <khairuldinsuyitno@gmail.com>
2026-04-02 02:28:37 +0800

    Create lokasi-orb.js

---
+++ lokasi-orb.js
@@ -0,0 +1,45 @@
+// Data lokasi Orb Malaysia
+const lokasiOrb = [
+  {
+    negeri: "Sarawak",
+    nama: "Summer Mall",
+    alamat: "Lot 7455, Block 1, Jln Datuk Mohammad Musa, 94300 Kota Samarahan, Sarawak",
+    maps: "https://maps.google.com/?q=Summer+Mall+Kota+Samarahan",
+    operator: true
+  },
+  {
+    negeri: "Kuala Lumpur",
+    nama: "Hometown Kota Raya",
+    alamat: "Kompleks Kota Raya, Kuala Lumpur",
+    maps: "https://maps.google.com/?q=Kompleks+Kota+Raya+Kuala+Lumpur",
+    operator: true
+  },
+  {
+    negeri: "Kelantan",
+    nama: "Tumpat",
+    alamat: "NO.PT 3591, DEPAN KLINIK BANDAR BARU, 16200 TUMPAT, Kelantan",
+    maps: "https://maps.google.com/?q=NO.PT+3591+Tumpat+Kelantan",
+    operator: true
+  },
+  {
+    negeri: "Selangor",
+    nama: "Kajang Metro Plaza",
+    alamat: "Lot G40, Jln Tun Abdul Aziz, Bandar Kajang, 43000 Kajang",
+    maps: "https://maps.google.com/?q=Kajang+Metro+Plaza",
+    operator: true
+  },
+  {
+    negeri: "Sabah",
+    nama: "Putatan Mall",
+    alamat: "G54, Ground Floor, Bandar Putatan, 89500 Kota Kinabalu, Sabah",
+    maps: "https://maps.google.com/?q=Putatan+Mall+Kota+Kinabalu",
+    operator: true
+  },
+  {
+    negeri: "Kuala Lumpur",
+    nama: "Sungai Wang",
+    alamat: "Level 3 - Lot T-078, Sungei Wang Plaza, Jln Sultan Ismail, Bukit Bintang, 55100 KL",
+    maps: "https://maps.google.com/?q=Sungei+Wang+Plaza+Kuala+Lumpur",
+    operator: true
+  }
+];
